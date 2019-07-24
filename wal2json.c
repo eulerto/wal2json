93fd69dd85f877b21dfb96d416fbbfb0483278b3
Mohammad Mahabobur Rahman <mahabobur.rahman@metafour.com>
2019-07-24 15:13:08 +0600

    Includes nulls in key values

--- wal2json.c
+++ wal2json.c
@@ -594,8 +594,8 @@ tuple_to_stringinfo(LogicalDecodingContext *ctx, TupleDesc tupdesc, HeapTuple tu
 		origval = heap_getattr(tuple, natt + 1, tupdesc, &isnull);
 
 		/* Skip nulls iif printing key/identity */
-		if (isnull && replident)
-			continue;
+		//if (isnull && replident)
+		//	continue;
 
 		typid = attr->atttypid;
 
