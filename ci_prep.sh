c1d974ac926e6150ac0278ddaa88b0fabbba577f
Greig Wise <fritalian@gmail.com>
2019-10-14 15:59:02 -0400

    DATA-483: Add ci prep script

---
+++ ci_prep.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $HOME/src/github.com/Hireology/wal2json
+USE_PGXS=1 make
+sudo USE_PGXS=1 make install
+
+psql -c "alter system set wal_level = 'logical'"
+psql -c "alter system set max_replication_slots = 1"
+sudo service postgresql restart
