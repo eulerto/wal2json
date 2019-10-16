68061a9954501918621cedb348c5abda705ec43a
Greig Wise <fritalian@gmail.com>
2019-10-16 11:54:13 -0400

    DATA-483: Change path for postgres 10

--- ci_prep.sh
+++ ci_prep.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=/usr/lib/postgresql/10/bin:$PATH
 cd $HOME/src/github.com/Hireology/wal2json
 USE_PGXS=1 make
 sudo USE_PGXS=1 make install
@@ -6,3 +7,4 @@ sudo USE_PGXS=1 make install
 psql -c "alter system set wal_level = 'logical'"
 psql -c "alter system set max_replication_slots = 1"
 sudo service postgresql restart
+
